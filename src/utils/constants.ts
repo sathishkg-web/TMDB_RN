feb7996bafb98b9e96a310f1e965ec4e73582613
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2026-07-22 04:01:06 +0530

    Add constants for image base URL and sizes

---
+++ src/utils/constants.ts
@@ -0,0 +1,7 @@
+//import Config from 'react-native-config';
+
+//export const IMAGE_BASE_URL = Config.TMDB_IMAGE_URL;
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+export const POSTER_SIZE = 'w500';
+export const BACKDROP_SIZE = 'w780';
