9bd2885e5f4f59829fc918b7474838860e1dc7c7
sathishkg-web <59190276+sathishkg-web@users.noreply.github.com>
2026-07-22 03:57:58 +0530

    Create colors.tsx

---
+++ src/theme/colors.tsx
@@ -0,0 +1,14 @@
+export const Colors = {
+    background: '#0F1115',
+    surface: '#1B1E24',
+    primary: '#FFB703',
+    secondary: '#8ECAE6',
+
+    text: '#FFFFFF',
+    textSecondary: '#B5B5B5',
+
+    success: '#4CAF50',
+    danger: '#F44336',
+
+    border: '#2D3138',
+};
